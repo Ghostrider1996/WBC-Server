57685dd62cb773363d28dd4107b3fbf34cb1ec3b
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2026-09-10 13:26:59 +0300

    server setup fix
    after physical separation of client end server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { configRoutes } = require("./config/configRouter");
 const { configExpress } = require("./config/configExpress");
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const PORT = process.env.PORT || 3030;
 const app = express();
